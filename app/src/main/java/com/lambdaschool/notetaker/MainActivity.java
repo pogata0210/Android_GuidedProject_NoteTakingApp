c4d25f3089c492d4c8c24d2ef73596fb3d97a7eb
Chance Payne <chance@lambdaschool.com>
2018-11-06 12:05:08 -0700

    initial commit, S01M02

---
+++ app/src/main/java/com/lambdaschool/notetaker/MainActivity.java
@@ -0,0 +1,45 @@
+package com.lambdaschool.notetaker;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Long> notes;
+    private Context         context;
+    private LinearLayout    listLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notes = new ArrayList<>();
+        context = this;
+        listLayout = findViewById(R.id.list_layout);
+
+        findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notes.add(System.currentTimeMillis());
+                listLayout.addView(getDefaultTextView(notes.get(notes.size() - 1).toString()));
+                Log.i(getLocalClassName(), notes.toString());
+            }
+        });
+    }
+
+    private TextView getDefaultTextView(String text) {
+        TextView textView = new TextView(context);
+        textView.setText(text);
+        textView.setTextSize(24);
+        textView.setPadding(10, 10, 10, 10);
+        return textView;
+    }
+}
